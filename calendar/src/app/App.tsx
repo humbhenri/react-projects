41071b640eaac7913362279e016b116acdaa5e8e
Humberto Pinheiro <humbhenri@gmail.com>
2021-11-18 16:06:15 -0300

    cria tela

--- calendar/src/app/App.tsx
+++ calendar/src/app/App.tsx
@@ -1,46 +1,9 @@
 import './App.css';
-import Box from '@material-ui/core/Box';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
-
-const rows = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex'];
+import CalendarScreen from './CalendarScreen';
 
 function App() {
-  const classes = useStyles();
   return (
-    <Box display="flex" alignItems="stretch">
-      <Box>
-        <h1>Test</h1>
-      </Box>
-      <TableContainer component={"div"}>
-      <Table className={classes.table} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-          {rows.map((row) => (
-              <TableCell component="th" scope="row">
-                {row}
-              </TableCell>
-          ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          
-        </TableBody>
-      </Table>
-    </TableContainer>
-    </Box>
+      <CalendarScreen />
   );
 }
 
---
+++ calendar/src/app/CalendarScreen.tsx
@@ -0,0 +1,99 @@
+import Box from "@material-ui/core/Box";
+import { makeStyles } from "@mui/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Button from "@material-ui/core/Button";
+import Checkbox from "@mui/material/Checkbox";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+
+const useStyles = makeStyles((theme?: any) => ({
+  table: {
+    height: "100%",
+    "& td ~ td, & th ~ th": {
+      borderLeft: "1px solid rgb(224, 224, 224)",
+    },
+  },
+  tbody: {
+    height: "100%",
+  },
+  agenda: {
+    minWidth: "16em",
+    padding: "8px 16px",
+  },
+}));
+
+const rows = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
+export default function CalendarScreen() {
+  const classes = useStyles();
+  return (
+    <Box display="flex" height="100%">
+      <Box className={classes.agenda}>
+        <h1>Agenda</h1>
+        <Button variant="contained" color="primary">
+          Novo evento
+        </Button>
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox name="agenda 1" />}
+            label="Agenda 1"
+          />
+          <FormControlLabel
+            control={<Checkbox name="agenda 2" />}
+            label="Agenda 2"
+          />
+        </FormGroup>
+      </Box>
+      <TableContainer component={"div"}>
+        <Table
+          size="small"
+          aria-label="a dense table"
+          className={classes.table}
+        >
+          <TableHead>
+            <TableRow>
+              {rows.map((row) => (
+                <TableCell align="center" key={row}>
+                  {row}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody className={classes.tbody}>
+            <TableRow>
+              <TableCell align="center">x</TableCell>
+              <TableCell align="center">x</TableCell>
+              <TableCell align="center">x</TableCell>
+              <TableCell align="center">x</TableCell>
+              <TableCell align="center">x</TableCell>
+              <TableCell align="center">x</TableCell>
+              <TableCell align="center">x</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell align="center">x</TableCell>
+              <TableCell align="center">x</TableCell>
+              <TableCell align="center">x</TableCell>
+              <TableCell align="center">x</TableCell>
+              <TableCell align="center">x</TableCell>
+              <TableCell align="center">x</TableCell>
+              <TableCell align="center">x</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell align="center">x</TableCell>
+              <TableCell align="center">x</TableCell>
+              <TableCell align="center">x</TableCell>
+              <TableCell align="center">x</TableCell>
+              <TableCell align="center">x</TableCell>
+              <TableCell align="center">x</TableCell>
+              <TableCell align="center">x</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+}
